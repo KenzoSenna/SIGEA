da9ea3fccb61e83af179dee0ea457d4e733e2e85
Matheus Camarotto Manfio Domingues <ext.matheus.domingues@eldoradobrasil.com.br>
2026-05-14 17:04:29 -0400

    feat: adição de classes de reserva por semestre e diaria, com diferenciação de inputs para cada tipo de reserva

--- SIGEA/_routes.py
+++ SIGEA/_routes.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI, APIRouter, HTTPException
-from datetime import datetime, date, time
-from pydantic import BaseModel
+from datetime import date, time
+from pydantic import BaseModel, model_validator
+from typing import Union, Literal
 import uvicorn
- 
+
 app = FastAPI()
 router = APIRouter(prefix="/auth", tags=["Autenticação"])
  
 class LoginRequest(BaseModel):
     email: str
     senha: str
-    role: int
+    #role: int
 
 class ReservaSemestral(BaseModel):
     data_inicio: date
     data_fim: date
     horario_inicio: time
     horario_fim: time
+
 class ReservaDiaria(BaseModel):
     data: date
     horario_inicio: time
@@ -24,18 +26,23 @@ class ReservaDiaria(BaseModel):
 class ReservaRequest(BaseModel):
     id_reserva: int
     id_sala: int
-    semestral: bool
     id_disciplina: int
-    datetime_inicio: datetime
-    datetime_fim: datetime
     descricao: str
+    tipo_reserva: Literal["diaria", "semestral"]  # Garante apenas estas duas opções
+    detalhes: Union[ReservaDiaria, ReservaSemestral]  # Aceita um modelo ou o outro
 
-
+    def verificar_tipo_detalhes(self):
+        if self.tipo_reserva == "diaria" and not isinstance(self.detalhes, ReservaDiaria):
+            raise ValueError("Para 'tipo_reserva' igual a 'diaria', insira os campos de ReservaDiaria.")
+        if self.tipo_reserva == "semestral" and not isinstance(self.detalhes, ReservaSemestral):
+            raise ValueError("Para 'tipo_reserva' igual a 'semestral', insira os campos de ReservaSemestral.")
+        return self
+    
 EMAIL = "usuario@email.com"
 SENHA = "senha123"
-ROLE_PROFESSOR = 0
-ROLE_ALUNO = 1
-salas = {}
+# ROLE_PROFESSOR = 0
+# ROLE_ALUNO = 1
+salas = [201, 202, 204]
 reservas = {}
 
 @router.post("/login", summary="Fazer login")
@@ -48,17 +55,36 @@ async def login(dados: LoginRequest):
 
 @router.post("/reservas", summary="Realizar reserva de sala")
 async def create_reserva(dados: ReservaRequest):
-    if dados.id_sala in salas:
-        if dados.semestral == True:
-            pass
+    if dados.id_sala not in salas:
+        raise HTTPException(status_code=404, detail={"sucesso": False, "mensagem": "Sala não encontrada"}) 
+    if dados.tipo_reserva == "diaria":
         reservas[dados.id_reserva] = {
-            "datetime_inicio": dados.datetime_inicio,
-            "datetime_fim": dados.datetime_fim, 
+            "id_sala": dados.id_sala,
             "id_disciplina": dados.id_disciplina,
             "descricao": dados.descricao,
-            "id_sala": dados.id_sala
-            }
+            "tipo_reserva": dados.tipo_reserva,
+            "data": dados.detalhes.data,
+            "horario_inicio": dados.detalhes.horario_inicio,
+            "horario_fim": dados.detalhes.horario_fim,
+        }
 
+    elif dados.tipo_reserva == "semestral":
+        reservas[dados.id_reserva] = {
+            "id_sala": dados.id_sala,
+            "id_disciplina": dados.id_disciplina,
+            "descricao": dados.descricao,
+            "tipo_reserva": dados.tipo_reserva,
+            "data_inicio": dados.detalhes.data_inicio,
+            "data_fim": dados.detalhes.data_fim,
+            "horario_inicio": dados.detalhes.horario_inicio,
+            "horario_fim": dados.detalhes.horario_fim,
+        }
+
+    return {
+        "sucesso": True, 
+        "mensagem": f"Reserva {dados.tipo_reserva} realizada com sucesso!",
+        "dados_salvos": reservas[dados.id_reserva]
+    }        
 app.include_router(router)
  
 if __name__ == "__main__":

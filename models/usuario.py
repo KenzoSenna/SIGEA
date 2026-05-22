684d5aabb43b42c547c1a3c3e822f7e516937608
KenzoSenna <kenzoandblade@gmail.com>
2026-05-22 01:03:30 -0400

    fix: correção de enum pra typechar()

--- models/usuario.py
+++ models/usuario.py
@@ -15,7 +15,7 @@ class Usuario(Base):
     nome = Column(String(100), nullable=False)
     email = Column(String(150), unique=True, nullable=False, index=True)
     senha_hash = Column(String(255), nullable=False)
-    tipo = Column(Enum(TipoUsuario), nullable=False, default=TipoUsuario.ALUNO)
+    tipo = Column(String(20), nullable=False)
     created_at = Column(DateTime, nullable=False, server_default=func.now())
     updated_at = Column(DateTime, nullable=False, server_default=func.now(), onupdate=func.now())
 
